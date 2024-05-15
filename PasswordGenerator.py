f92db132548cc145f678c098307240257a268998
ElevatorsCS <willf4@vt.edu>
2024-05-14 21:35:44 -0400

    Update PasswordGenerator.py
    Added print function.

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,8 +1,10 @@
 import random
 import string
 
-def password_generator(length: int=8):
+def password_generator(length: int=12):
     alphabet = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(alphabet) for i in range(length))
     return password
-
+    
+password = password_generator()
+print(f"Random Password: {password}")
